4a1e0ca517bd2bf97d64074dac5dd2a76a9526b0
liwei <liwei@iautorun.com>
2022-02-21 20:19:05 +0800

    添加说明

---
+++ docker/conf/init/m1.sql
@@ -0,0 +1,19 @@
+create database order_db character set utf8mb4 COLLATE utf8mb4_unicode_ci;
+use order_db;
+CREATE TABLE `t_order_1`
+(
+    `order_id` BIGINT(19)     NOT NULL COMMENT '订单id',
+    `price`    DECIMAL(10, 2) NOT NULL COMMENT '订单价格',
+    `user_id`  BIGINT(19)     NOT NULL COMMENT '下单用户id',
+    `status`   VARCHAR(50)    NOT NULL COMMENT '订单状态' COLLATE 'utf8_general_ci',
+    PRIMARY KEY (`order_id`) USING BTREE
+);
+
+CREATE TABLE `t_order_2`
+(
+    `order_id` BIGINT(19)     NOT NULL COMMENT '订单id',
+    `price`    DECIMAL(10, 2) NOT NULL COMMENT '订单价格',
+    `user_id`  BIGINT(19)     NOT NULL COMMENT '下单用户id',
+    `status`   VARCHAR(50)    NOT NULL COMMENT '订单状态' COLLATE 'utf8_general_ci',
+    PRIMARY KEY (`order_id`) USING BTREE
+);
